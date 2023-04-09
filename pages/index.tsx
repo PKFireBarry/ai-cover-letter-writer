fdd2c53002ef0a017150d626c04a58bd5f8805bc
Darion George <Barry0719@gmail.com>
2023-04-09 18:35:15 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
     window.location.reload();
   };
 
-  if (!isAuth == true) {
+  if (!auth.currentUser) {
     return (
       <div>
         <Login setIsAuth={setIsAuth} />
